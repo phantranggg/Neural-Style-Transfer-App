2efe3b9856f641dbccf1db0346c9ff7e3440ab45
phantrang <phantrang3006@gmail.com>
2019-03-17 21:29:10 +0700

    Change folder directory

--- src/upload2.py
+++ src/upload2.py
@@ -3,14 +3,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = os.path.basename('uploads')
+# UPLOAD_FOLDER = os.path.basename('static/uploads')
+UPLOAD_FOLDER = os.getcwd() + '/static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -19,5 +18,7 @@ def upload_file():
 
     # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
     file.save(f)
-
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8001)
