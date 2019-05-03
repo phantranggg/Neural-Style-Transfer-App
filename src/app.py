3183f8cc672dcc9c0e16f7f415fc6bc6ac4ac6be
tung <tungts1101@gmail.com>
2019-05-03 22:00:49 +0700

    upload default style for newcomers & using different structure for tkinter app

--- src/app.py
+++ src/app.py
@@ -1,62 +1,122 @@
-import tkinter as tk
-from PIL import Image, ImageTk
+from tkinter import *
 from tkinter import filedialog as tkfd
+from PIL import Image, ImageTk
 import os
 
-root = tk.Tk()
-root.title("Neural style transfer")
+contentImage = None
+styleImage = None
 
-canvas = tk.Canvas(root,width=1200,height=600)
-canvas.pack()
+top = Tk()
+top.geometry("1200x600")
+top.resizable(0,0)
 
 dirname = os.path.dirname(__file__)
+
+popularStylePath = os.path.join(dirname, './static/popular_style/')
+
+def createImage(imgPath,size):
+    try:
+        img = Image.open(imgPath)
+        img = img.resize(size,Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        return img
+    except Exception as ex:
+        print(ex)
+
+class PopularStyle:
+    def __init__(self,imgName,pWidget,x,y):
+        self.img = createImage(popularStylePath+imgName,size=(125,125))
+        self.btn = Button(pWidget,image=self.img,command=self.setStyle)
+        self.btn.place(x=x,y=y)
+    
+    def setStyle(self):
+        global styleImage
+        styleImage = self.img
+
+
 imgPath = os.path.join(dirname, './static/')
 
-class Block:
-	def __init__(self,root):
-		img = Image.open(imgPath+'no-image.png')
-		img = img.resize((300,300),Image.ANTIALIAS)
-		self.img = ImageTk.PhotoImage(img)
-		self.label = tk.Label(root,image=self.img)
-		self.btn = tk.Button(root,text="Choose image",command=self.choose_img)
-	
-	def choose_img(self):
-		f = tkfd.askopenfile(initialdir = imgPath,title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-		try:
-			# TODO: check image in here
-			img = Image.open(f)
-			img = img.resize((300,300),Image.ANTIALIAS)
-			self.img = ImageTk.PhotoImage(img)
-			self.label.configure(image=self.img)
-		except (Exception, e):
-			print(str(e))
-			pass
+class Upload:
+    def __init__(self,imgName,pWidget,imgType):
+        self.imgType = imgType
+        self.img = createImage(imgPath+imgName,size=(300,300))
+        self.btn = Button(pWidget,image=self.img,command=self.uploadImage)
+        self.btn.pack()
+    
+    def uploadImage(self):
+        global styleImage,contentImage
+        f = tkfd.askopenfile(initialdir = imgPath,title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        try:
+            self.img = createImage(f.name,size=(300,300))
+            self.btn.configure(image=self.img)
+            if self.imgType == 'content':
+                contentImage = self.img
+            elif self.imgType == 'style':
+                styleImage = self.img
+        except Exception as ex:
+            print(ex)
+            pass
 
-content = Block(canvas)
-style = Block(canvas)
+# content
+cUploadContent = Canvas(top,height=300,width=300)
+cUploadContent.place(x=50,y=100)
 
-# specify image and button location
-content.label.place(x=0,rely=0.1)
-content.btn.place(x=100,y=450)
+uploadStyle = Upload('no-image.png',cUploadContent,imgType='content')
+contentLabel = Label(top,text="Content Image")
+contentLabel.place(x=170,y=420)
+# =============
 
-style.label.place(x=400,rely=0.1)
-style.btn.place(x=500,y=450)
+# style
+styleFlag = True    # open popular style
+def togglePopularStyle():
+    global styleFlag
+    styleFlag = True
+    if styleFlag:
+        Misc.lift(cPopularStyle)
+
+def toggleUploadStyle():
+    global styleFlag
+    styleFlag = False
+    if not styleFlag:
+        Misc.lift(cUploadStyle)
+
+popularStyleBtn = Button(top,text="Popular Style",command=togglePopularStyle)
+popularStyleBtn.place(x=400,y=50)
+
+uploadStyleBtn = Button(top,text="Upload style",command=toggleUploadStyle)
+uploadStyleBtn.place(x=550,y=50)
+
+cUploadStyle = Canvas(top,height=300,width=300)
+cUploadStyle.place(x=400,y=100)
+
+cPopularStyle = Canvas(top,height=300,width=300)
+cPopularStyle.place(x=400,y=100)
+
+styleLabel = Label(top,text="Style Image")
+styleLabel.place(x=520,y=420)
+
+uploadStyle = Upload('no-image.png',cUploadStyle,imgType='style')
+
+style_1 = PopularStyle('1.jpg',cPopularStyle,x=10,y=10)
+style_2 = PopularStyle('2.jpg',cPopularStyle,x=165,y=10)
+style_3 = PopularStyle('3.jpg',cPopularStyle,x=10,y=165)
+style_4 = PopularStyle('4.jpg',cPopularStyle,x=165,y=165)
+
+# ==============
 
 def transfer():
-        #TODO: pass function in module model
-	# use content.img & style.img as 2 inputs
+    #TODO: pass function in module model
+	# use contentImage & styleImage as 2 inputs
 	print("-----")
 
-f = Image.open(imgPath+'arrow.png')
-f = f.resize((100,100),Image.ANTIALIAS)
-transferIcon = ImageTk.PhotoImage(f)
-transferBtn = tk.Button(canvas,image=transferIcon,text="Transfer",command=transfer)
-transferBtn.place(x=750,y=150)
+transferIcon = createImage(imgPath+'arrow.png',size=(100,100))
+transferBtn = Button(top,image=transferIcon,text="Transfer",command=transfer)
+transferBtn.place(x=735,y=200)
 
-f = Image.open(imgPath+'no-image.png')
-f = f.resize((300,300),Image.ANTIALIAS)
-generatedImg = ImageTk.PhotoImage(f)
-transferLabel = tk.Label(canvas,image=generatedImg)
-transferLabel.place(x=900,rely=0.1)
+generatedImg = createImage(imgPath+'no-image.png',size=(300,300))
+transferLabel = Label(top,image=generatedImg)
+transferLabel.place(x=875,y=100)
+generatedLabel = Label(top,text="Transfer Image")
+generatedLabel.place(x=1000,y=420)
 
-root.mainloop()
+top.mainloop()
